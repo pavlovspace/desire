451f390d25d7f9c2a29273184e7b055b2c89b27f
Paul Pavlov <pavlov1space@gmail.com>
2023-06-08 23:40:04 +0200

    Implemented - right slide menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,40 @@
-$(function () {
+let headerBtn = document.querySelector('.header__btn');
+let rightsideMenu = document.querySelector('.rightside-menu');
+let BtnClose = document.querySelector('.rightside-menu__close');
 
+headerBtn.addEventListener('click', function(){
+    rightsideMenu.classList.remove('rightside-menu--close')
 })
+
+BtnClose.addEventListener('click', function(){
+    rightsideMenu.classList.add('rightside-menu--close')
+})
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
